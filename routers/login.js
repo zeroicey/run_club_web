93920443b58e59b159bdc7f040aea61e53745a45
alonescar <alonescar.dhl@gmail.com>
2023-01-23 19:53:07 +0800

    update database

--- routers/login.js
+++ routers/login.js
@@ -4,34 +4,34 @@ const conn = require('../database')
 router = express.Router()
 
 router
-    .get('/login', (req, res) => {
-        res.render('login.html')
+  .get('/login', (req, res) => {
+    res.render('login.html')
+  })
+  .post('/login', (req, res) => {
+    login_data = req.body
+    conn.query(`SELECT password, nickname FROM users where username="${login_data.username}"`, (err, ret) => {
+      if (err) {
+        console.log('Database error');
+        res.send(false)
+      }
+      // console.log(ret);
+      else {
+        try {
+          if (ret[0].password === login_data.password) {
+            req.session.user = login_data
+            conn.query(`UPDATE users SET lastLoginTime='${new Date().getTime()}' WHERE username='${login_data.username}'`, (err, ret) => {
+              if (err) { console.log(err); return res.send(false) }                    
+              return res.send(true)
+            })
+          } else {
+            res.send(false)
+          }
+        } catch (error) {
+          console.log(error);
+          return res.send(false)
+        }
+      }
     })
-    .post('/login', (req, res) => {
-        login_data = req.body
-        conn.query(`SELECT password, nickname FROM users where username="${login_data.username}"`, (err, ret) => {
-            if (err) {
-                console.log('Database error');
-                res.send(false)
-            }
-            // console.log(ret);
-            else {
-                try {
-                    if (ret[0].password === login_data.password) {
-                        req.session.user = login_data
-                        conn.query(`UPDATE users SET lastLoginTime='${new Date().getTime()}' WHERE username='${login_data.username}'`, (err, ret) => {
-                            if (err) { console.log(err); return res.send(false) }                    
-                            return res.send(true)
-                        })
-                    } else {
-                        res.send(false)
-                    }
-                } catch (error) {
-                    console.log(error);
-                    return res.send(false)
-                }
-            }
-        })
-    })
+  })
 
 module.exports = router
--- routers/user.js
+++ routers/user.js
@@ -5,110 +5,110 @@ const { PicGo } = require('picgo')
 const picgo = new PicGo()
 
 async function picUpload(picPath) {
-    return await picgo.upload([picPath]);
+  return await picgo.upload([picPath]);
 }
 
 router = express.Router()
 
 router
-    .get('/user', (req, res) => {
-        if (req.session.user ? false : true) { return res.redirect('/login') }
-        conn.query(`SELECT password, nickname, username, class FROM users where username="${req.session.user.username}"`, (err, ret0) => {
-            if (err) {
-                console.log(err);
-                res.send(false)
-            } else {
-                conn.query(`SELECT * FROM cards where username='${req.session.user.username}' ORDER BY id`, (err, ret1) => {
-                    if (err) {
-                        console.log(err);
-                        res.send(false)
-                    } else {
-                        ret0[0]._class = ret0[0].class
-                        // console.log(ret1);
-                        ret1.forEach(element => {
-                            let date = new Date(element.date)
-                            element.date = date.getFullYear() + '-' + (date.getMonth() + 1) + '-' + date.getDate()
-                        });
-                        res.render('user.html', {user: ret0[0], cards: ret1})
-                    }
-                })
+  .get('/user', (req, res) => {
+    if (req.session.user ? false : true) { return res.redirect('/login') }
+    conn.query(`SELECT password, nickname, username, class FROM users where username="${req.session.user.username}"`, (err, ret0) => {
+      if (err) {
+        console.log(err);
+        res.send(false)
+      } else {
+        conn.query(`SELECT * FROM cards where username='${req.session.user.username}' ORDER BY id`, (err, ret1) => {
+          if (err) {
+            console.log(err);
+            res.send(false)
+          } else {
+            ret0[0]._class = ret0[0].class
+            // console.log(ret1);
+            ret1.forEach(element => {
+              let date = new Date(element.date)
+              element.date = date.getFullYear() + '-' + (date.getMonth() + 1) + '-' + date.getDate()
+            });
+            res.render('user.html', {user: ret0[0], cards: ret1})
+          }
+        })
 
-            }
-        })
+      }
     })
-    .post('/user', (req, res) => {
-        if (req.session.user ? false : true) { return res.redirect('/login') }
-        let data = req.body
-        // console.log(data);
-        conn.query(`SELECT password, nickname, class FROM users where username="${req.session.user.username}"`, (err, ret) => {
-            if (err) {
-                console.log(err);
-                res.send(false)
-            } else {
-                if (data.class !== ret[0].class || data.password !== ret[0].password || data.nickname !== ret[0].nickname) {
-                    conn.query(`UPDATE users SET password='${data.password}', class='${data.class}', nickname='${data.nickname}' WHERE username='${req.session.user.username}'`, (err) => {
-                        if (err) { console.log(err); return res.send(false) }                    
-                        else {
-                            if (data.password !== ret[0].password) { req.session.user = null }
-                            return res.send(true)
-                        }
-                    })
-                } else {
-                    res.send(false)
-                }
+  })
+  .post('/user', (req, res) => {
+    if (req.session.user ? false : true) { return res.redirect('/login') }
+    let data = req.body
+    // console.log(data);
+    conn.query(`SELECT password, nickname, class FROM users where username="${req.session.user.username}"`, (err, ret) => {
+      if (err) {
+        console.log(err);
+        res.send(false)
+      } else {
+        if (data.class !== ret[0].class || data.password !== ret[0].password || data.nickname !== ret[0].nickname) {
+          conn.query(`UPDATE users SET password='${data.password}', class='${data.class}', nickname='${data.nickname}' WHERE username='${req.session.user.username}'`, (err) => {
+            if (err) { console.log(err); return res.send(false) }                    
+            else {
+              if (data.password !== ret[0].password) { req.session.user = null }
+              return res.send(true)
             }
-        })
+          })
+        } else {
+          res.send(false)
+        }
+      }
     })
-    .post('/reloadPic', (req, res) => {
-        if (req.session.user ? false : true) { return res.redirect('/login') }
-        let data = req.body
-        conn.query(`SELECT isPic1, isPic2, pic1Url, pic2Url FROM cards WHERE id=${data.id}`, async (err, [ret]) => {
-            if (err) {
-                console.log(err);
-                res.send(false)
-            } else {
-                if (ret.isPic1 === 1) {
-                    data.pic1_path = path.join(__dirname, '../temp', ret.pic1Url.split('/')[(ret.pic1Url.split('/')).length-1])
-                    try {
-                        await picUpload(data.pic1_path)
-                    } catch (error) {
-                        console.log(error);
-                        res.send(false)
-                    }
-                }
-                if (ret.isPic2 === 1) {
-                    data.pic2_path = path.join(__dirname, '../temp', ret.pic2Url.split('/')[(ret.pic2Url.split('/')).length-1])
-                    try {
-                        await picUpload(data.pic2_path)
-                    } catch (error) {
-                        console.log(error);
-                        res.send(false)
-                    }
-                }
-                res.send(true)
-            }
-        })
-    })
-    .post('/deleteCard', (req, res) => {
-        if (req.session.user ? false : true) { return res.redirect('/login') }
-        let data_req = req.body
-        conn.query(`SELECT clockNum, integration FROM users WHERE username='${req.session.user.username}'`, (err, ret) => {
-            if (err) {console.log(err); return res.send(false)}
-            conn.query(`SELECT len, isPic1, isPic2 FROM cards WHERE username='${req.session.user.username}'`, (err, [data]) => {
-                console.log(data);
-                if (err) {console.log(err); return res.send(false)}
-                conn.query(`UPDATE users SET integration=${ret[0].integration - 5 - (parseFloat((data.len).split(' ')[0]) + parseFloat((data.len).split(' ')[1] / 100)) * 2 - data.isPic1 * 3 - data.isPic2 * 3}
-                    , clockNum=${ret[0].clockNum - 1} where username='${req.session.user.username}'`, (err) => {
-                    if (err) {console.log(err); return res.send(false)}
-                    else {
-                        conn.query(`DELETE FROM cards WHERE id='${data_req.id}'`, (err, ret) => {
-                            if (err) {console.log(err); return res.send(false)}
-                        })
-                    }
-                })
-            })
-        })
+  })
+  .post('/reloadPic', (req, res) => {
+    if (req.session.user ? false : true) { return res.redirect('/login') }
+    let data = req.body
+    conn.query(`SELECT isPic1, isPic2, pic1Url, pic2Url FROM cards WHERE id=${data.id}`, async (err, [ret]) => {
+      if (err) {
+        console.log(err);
+        res.send(false)
+      } else {
+        if (ret.isPic1 === 1) {
+          data.pic1_path = path.join(__dirname, '../temp', ret.pic1Url.split('/')[(ret.pic1Url.split('/')).length-1])
+          try {
+            await picUpload(data.pic1_path)
+          } catch (error) {
+            console.log(error);
+            res.send(false)
+          }
+        }
+        if (ret.isPic2 === 1) {
+          data.pic2_path = path.join(__dirname, '../temp', ret.pic2Url.split('/')[(ret.pic2Url.split('/')).length-1])
+          try {
+            await picUpload(data.pic2_path)
+          } catch (error) {
+            console.log(error);
+            res.send(false)
+          }
+        }
         res.send(true)
+      }
     })
+  })
+  .post('/deleteCard', (req, res) => {
+    if (req.session.user ? false : true) { return res.redirect('/login') }
+    let data_req = req.body
+    conn.query(`SELECT clockNum, integration FROM users WHERE username='${req.session.user.username}'`, (err, ret) => {
+      if (err) {console.log(err); return res.send(false)}
+      conn.query(`SELECT len, isPic1, isPic2 FROM cards WHERE username='${req.session.user.username}'`, (err, [data]) => {
+        console.log(data);
+        if (err) {console.log(err); return res.send(false)}
+        conn.query(`UPDATE users SET integration=${ret[0].integration - 5 - (parseFloat((data.len).split(' ')[0]) + parseFloat((data.len).split(' ')[1] / 100)) * 2 - data.isPic1 * 3 - data.isPic2 * 3}
+, clockNum=${ret[0].clockNum - 1} where username='${req.session.user.username}'`, (err) => {
+            if (err) {console.log(err); return res.send(false)}
+            else {
+              conn.query(`DELETE FROM cards WHERE id='${data_req.id}'`, (err, ret) => {
+                if (err) {console.log(err); return res.send(false)}
+              })
+            }
+          })
+      })
+    })
+    res.send(true)
+  })
 
 module.exports = router
